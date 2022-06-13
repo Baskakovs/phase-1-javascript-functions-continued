aedc37fbe819fc807d6aed3598528ba5e12e3f4e
Pjotrs Baskakovs <baskakovs@me.com>
2022-06-13 18:35:33 +0300

    commit tree 28328b78a7155ade6015218659cfdf2757f9160e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(string= "roller-skate"){
+    return(`This Saturday, I want to ${string}!`)
+}
+
+const mondayWork = function(string = "go to the office"){
+    return(`This Monday, I will ${string}.`)
+}
+
+function wrapAdjective(visualFlair= "*"){
+    return function(adjective = "special"){
+        return (`You are ${visualFlair}${adjective}${visualFlair}!`)
+    }
+}
